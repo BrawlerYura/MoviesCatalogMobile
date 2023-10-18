b9a1ed8de94b65bd34670ea16cbf86b231e234ce
BrawlerYura <sitdikov.yuriy@gmail.com>
2023-10-18 22:07:32 +0700

    merged 19336 19338 19339 19340 19341 19404

--- app/src/main/java/com/example/mobile_moviescatalog2023/MainActivity.kt
+++ app/src/main/java/com/example/mobile_moviescatalog2023/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobile_moviescatalog2023.ui.theme.MOBILEMOVIESCATALOG2023Theme
+import com.example.mobile_moviescatalog2023.ui.theme.FilmusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MOBILEMOVIESCATALOG2023Theme {
+            FilmusTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MOBILEMOVIESCATALOG2023Theme {
+    FilmusTheme {
         Greeting("Android")
     }
 }
